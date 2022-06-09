966f64930faa74c3dfa434e8064fb71f15990bee
Antonio Espinosa <antespi@gmail.com>
2022-06-10 01:39:20 +0200

    Cookie banner

---
+++ js/cookie.notice.config.js
@@ -0,0 +1,48 @@
+new cookieNoticeJS({
+    
+    // Localizations of the notice message
+    'messageLocales': {
+      'es': 'Usamos cookies de terceros para analizar el tráfico de la página',
+      'en': 'We use third party cookies to analyze site traffic'
+    },
+   
+    // Localizations of the dismiss button text
+    'buttonLocales': {
+      'es': 'Aceptar',
+      'en': 'Accept'
+    },
+    
+    // Position for the cookie-notifier (default=bottom)
+    'cookieNoticePosition':'bottom',
+    
+    // Shows the "learn more button (default=false)
+    'learnMoreLinkEnabled':true,
+    
+    // The href of the learn more link must be applied if (learnMoreLinkEnabled=true) 
+    'learnMoreLinkHref':'cookies.html',
+    
+    // Text for optional learn more button
+    'learnMoreLinkText':{
+        'es':'Saber más',
+        'en':'Learn more'
+    },
+    
+    // The message will be shown again in X days
+    'expiresIn': 30, 
+    
+    // Dismiss button background color
+    'buttonBgColor': '#d35400',  
+    
+    // Dismiss button text color
+    'buttonTextColor': '#fff', 
+      
+    // Notice background color
+    'noticeBgColor': '#000', 
+       
+    // Notice text color
+    'noticeTextColor': '#fff', 
+    
+    // the lernMoreLink color (default='#009fdd') 
+    'linkColor':'#f00'
+       
+ });
